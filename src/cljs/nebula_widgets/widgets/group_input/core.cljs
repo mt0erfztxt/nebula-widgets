898455a254724d8cc3625d4c7f3790a8bd4fb11b
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-08-05 21:26:39 +0500

    add 'errors' and 'invalid' props to group input and remove 'errors' from group input item

--- src/cljs/nebula_widgets/widgets/group_input/core.cljs
+++ src/cljs/nebula_widgets/widgets/group_input/core.cljs
@@ -4,6 +4,8 @@
     [reagent.core :as r]))
 
 (def ^:private bem "nw-groupInput")
+(def ^:private bem-error (str bem "__error"))
+(def ^:private bem-errors (str bem "__errors"))
 (def ^:private bem-inner (str bem "__inner"))
 (def ^:private bem-item (str bem "__item"))
 
@@ -30,7 +32,9 @@
     :cns               - optional, any, no default, component namespace
     :disabled          - optional, logical true/false, no default
     :equidistant       - optional, logical true/false, no default, whether items have same width
+    :errors            - optional, seq of strings, no default, would be displayed only when not empty and :invalid
     :inline            - optional, logical true/false, no default, whether items grouped stacked or inline
+    :invalid           - optional, logical true/false, no default, whether widget is in invalid state or not
     :item-props        - optional, map, no default, common props for all items in group, for example, event handlers
     :size              - optional, one of :large, :normal (default), :small or their string/symbol equivalents
     :soft-columns      - optional, logical true/false, no default, when logical true and :columns is also set then
@@ -41,11 +45,14 @@
     :widget            - optional, any, item widget, see concrete group input implementation for details
   children - optional, seq of renderables"
   [& _args]
-  (let [[{:keys [columns soft-columns] :as props} children] ((juxt r/props r/children) (r/current-component))]
+  (let [[{:keys [columns errors invalid soft-columns] :as props} children] ((juxt r/props r/children) (r/current-component))]
     [:div {:class (build-class props)}
      (into [:div {:class bem-inner}]
            (for [{:keys [checked] :as item} children]
              [:div (cond-> (assoc {} :class (bem-utils/build-class bem-item [["checked" checked]]))
                            (and (integer? columns) (pos? columns))
                            (update :style assoc (if soft-columns :min-width :width) (str (/ 100 columns) "%")))
-              item]))]))
+              item]))
+     (when (and invalid (seq errors))
+       (into [:ul {:class bem-errors}]
+             (for [error errors] [:li {:class bem-error} error])))]))
--- src/cljs/nebula_widgets/widgets/group_input/item.cljs
+++ src/cljs/nebula_widgets/widgets/group_input/item.cljs
@@ -3,22 +3,20 @@
     [nebula-widgets.utils.bem :as bem-utils]))
 
 (def ^:private bem "nw-groupInput-item")
-(def ^:private bem-error (str bem "-error"))
-(def ^:private bem-errors (str bem "__errors"))
 (def ^:private bem-input (str bem "__input"))
 (def ^:private bem-label (str bem "__label"))
 (def ^:private bem-label-text (str bem-label "-text"))
 
 (def ^:private non-input-props [:cid :cns :label :path :widget])
 
-(defn- build-class [{:keys [checked cid cns disabled ::invalid? widget]}]
+(defn- build-class [{:keys [checked cid cns disabled invalid widget]}]
   (bem-utils/build-class
     bem
     [["cns" cns]
      ["cid" cid]
      ["checked" checked]
      ["disabled" disabled]
-     ["invalid" invalid?]
+     ["invalid" invalid]
      ["widget" widget]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,23 +24,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; TODO: Maybe errors rendering is a responsibility of input component (see :input-cmp prop)?
+;;       Having invalid on item is convenient in UI testing.
 (defn widget
   "props          - required, map:
     :checked        - optional, logical true/false, no default
     :cid            - optional, any, no default, component id
     :cns            - optional, any, no default, component namespace
     :disabled       - optional, logical true/false, no default
-    :errors         - optional, seq of strings, no default
+    :invalid        - optional, logical true/false, no default
     :widget         - optional, any, item widget, see concrete group input item implementation for details
   input-cmp       - required, any component, e.g :input
   input-cmp-props - required, map, props for :input-cmp, any React supported props can be passed here"
-  [{:keys [errors label] :as props} input-cmp input-cmp-props]
-  (let [invalid? (boolean (and errors (seq errors)))
-        input-hcp [input-cmp (-> (apply dissoc input-cmp-props non-input-props) (assoc :class bem-input))]]
-    [:div {:class (-> props (assoc ::invalid? invalid?) build-class)}
+  [{:keys [label] :as props} input-cmp input-cmp-props]
+  (let [input-hcp [input-cmp (-> (apply dissoc input-cmp-props non-input-props) (assoc :class bem-input))]]
+    [:div {:class (build-class props)}
      (if label
        [:label {:class bem-label} input-hcp [:span {:class bem-label-text} label]]
-       input-hcp)
-     (when invalid?
-       (into [:ul {:class bem-errors}]
-             (for [error errors] [:li {:class bem-error} error])))]))
+       input-hcp)]))
