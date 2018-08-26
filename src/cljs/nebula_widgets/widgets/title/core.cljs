3b089293730662181c9459221861746b60c4238b
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-08-26 17:53:37 +0500

    make title widget BEM extendable

--- src/cljs/nebula_widgets/widgets/title/core.cljs
+++ src/cljs/nebula_widgets/widgets/title/core.cljs
@@ -3,11 +3,17 @@
     [nebula-widgets.utils.bem :as bem-utils]
     [reagent.core :as r]))
 
-(def ^:private bem "nw-title")
+(def ^:private default-bem
+  "nw-title")
 
-(defn- build-class [{:keys [cid size]}]
-  (bem-utils/build-class bem [["cid" cid]
-                              ["size" (-> size keyword #{:large :normal :small} (or :normal))]]))
+(defn- build-bem [bem]
+  (or bem default-bem))
+
+(defn- build-class [{:keys [bem cid size]}]
+  (bem-utils/build-class
+    (build-bem bem)
+    [["cid" cid]
+     ["size" (-> size keyword #{:large :normal :small} (or :normal))]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PUBLIC
@@ -15,6 +21,7 @@
 
 (defn widget [& _args]
   "props - optional map:
+    :bem  - optional, string, nw-title by default, would be used as widget's BEM
     :cid  - optional, any, no default, component id
     :size - optional, one of :large, :normal (default), :small or their string/symbol equivalents
   children - optional, seq of renderables, no default"
