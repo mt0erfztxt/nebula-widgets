0dc0c63fd7465bb40f14cda976650cfb2b08d3a4
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-09-08 19:49:18 +0500

    fix missing highlight.js dependency on global object

--- src/js/kitchen-sink/js-bundle.dev.js
+++ src/js/kitchen-sink/js-bundle.dev.js
@@ -2,6 +2,7 @@ var hljs = require('highlight.js/lib/highlight');
 var hljsClojure = require('highlight.js/lib/languages/clojure');
 var marked = require('marked');
 
+window.hljs = hljs;
 window.marked = marked;
 
 hljs.registerLanguage('clojure', hljsClojure);
