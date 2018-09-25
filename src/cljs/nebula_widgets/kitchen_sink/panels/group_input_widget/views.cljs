a951b4957b56d49b5a644737cb1463906853a995
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-09-25 23:33:00 +0500

    fix code formatting

--- src/cljs/nebula_widgets/kitchen_sink/panels/group_input_widget/views.cljs
+++ src/cljs/nebula_widgets/kitchen_sink/panels/group_input_widget/views.cljs
@@ -1,7 +1,8 @@
 (ns nebula-widgets.kitchen-sink.panels.group-input-widget.views
-  (:require [nebula-widgets.kitchen-sink.widgets.man-page.core :as man-page]
-            [nebula-widgets.kitchen-sink.widgets.markdown.core :as markdown]
-            [nebula-widgets.widgets.group-input.core :as group-input]))
+  (:require
+    [nebula-widgets.kitchen-sink.widgets.man-page.core :as man-page]
+    [nebula-widgets.kitchen-sink.widgets.markdown.core :as markdown]
+    [nebula-widgets.widgets.group-input.core :as group-input]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PUBLIC
