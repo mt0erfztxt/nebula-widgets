876593291e573af896672bf7dc263ee386f20b33
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-10-14 22:31:14 +0500

    simplified man page of radio group input item widget

--- src/cljs/nebula_widgets/kitchen_sink/panels/radio_group_input_item_widget/views.cljs
+++ src/cljs/nebula_widgets/kitchen_sink/panels/radio_group_input_item_widget/views.cljs
@@ -1,7 +1,6 @@
 (ns nebula-widgets.kitchen-sink.panels.radio-group-input-item-widget.views
   (:require
     [nebula-widgets.kitchen-sink.widgets.man-page.core :as man-page]
-    [nebula-widgets.kitchen-sink.widgets.markdown.core :as markdown]
     [nebula-widgets.widgets.radio-group-input.item :as radio-group-input-item]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -9,5 +8,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn widget []
-  [man-page/widget {:title "Radio group input item widget"}
-   [markdown/widget (-> #'radio-group-input-item/widget meta :doc)]])
+  [man-page/widget
+   "# Radio group input item widget"
+   (-> #'radio-group-input-item/widget meta :doc)])
