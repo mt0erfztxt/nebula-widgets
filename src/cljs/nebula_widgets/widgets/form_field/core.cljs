24392f4e1bf8f10fc75ea7906e174cfc15f804ef
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-10-17 00:53:47 +0500

    added form field widget

---
+++ src/cljs/nebula_widgets/widgets/form_field/core.cljs
@@ -0,0 +1,72 @@
+(ns nebula-widgets.widgets.form-field.core
+  (:require
+    [nebula-widgets.widgets.form-field.error :as form-field-error]
+    [nebula-widgets.utils.bem :as bem-utils]
+    [reagent.core :as r]))
+
+(def ^:private bem
+  "nw-formField")
+
+(def ^:private errors-elt-bem
+  (str bem "__errors"))
+
+(def ^:private errors-item-elt-bem
+  (str errors-elt-bem "-item"))
+
+(def ^:private input-elt-bem
+  (str bem "__input"))
+
+(def ^:private label-elt-bem
+  (str bem "__label"))
+
+(def ^:private label-aux-elt-bem
+  (str label-elt-bem "-aux"))
+
+(defn- build-class [{:keys [cid consider-input-margin disabled errors inline required]}]
+  (bem-utils/build-class
+    bem
+    [["cid" cid]
+     ["consider-input-margin" consider-input-margin]
+     ["disabled" disabled]
+     ["inline" inline]
+     ["invalid" (seq errors)]
+     ["required" required]]))
+
+(defn- label-elt-cmp [label]
+  [:div {:class label-elt-bem}
+   (if (coll? label)
+     [(first label) [:span {:class label-aux-elt-bem} (second label)]]
+     [label])])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PUBLIC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn widget
+  "Renders form field.
+
+  Arguments:
+  * `props` - optional, map, no default. Supported keys:
+    - `:cid` - any, no default. Component id.
+    - `:consider-input-margin` - logical true/false, no default. When logical true then field would have smaller bottom
+      padding, for example, it need to be set when field wraps group input component in which each child have bottom
+      margin/padding.
+    - `:disabled` - logical true/false, no default. Whether widget disabled or not.
+    - `:errors` - collection, no default. Errors to display.
+    - `:inline` - logical true/false, no default. Whether label must be placed before or above input.
+    - `:label` - string, tuple of strings, no default. Short description for field. When it's a tuple then first element
+      would be used as label's main text and second element as auxiliary text.
+    - `:required` - logical true/false, no default. When logical true then '*' would be appended to label.
+  * `& children` - optional, any number of child components"
+  [& _args]
+  (let [[{:keys [errors label] :as props} children] ((juxt r/props r/children) (r/current-component))]
+    [:div {:class (build-class props)}
+     (when (seq label)
+       [label-elt-cmp label])
+     (into [:div {:class input-elt-bem}] children)
+     (when (seq errors)
+       (into
+         [:ul {:class errors-elt-bem}]
+         (for [error errors]
+           [:li {:class errors-item-elt-bem}
+            [form-field-error/widget {:text error}]])))]))
