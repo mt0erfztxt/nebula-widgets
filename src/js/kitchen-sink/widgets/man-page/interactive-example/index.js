d329ca279bf4d426aea975d1c6c14572913c7794
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-12-02 02:39:01 +0500

    updated kitchen sink's man-page widgets to use state based fragment

--- src/js/kitchen-sink/widgets/man-page/interactive-example/index.js
+++ src/js/kitchen-sink/widgets/man-page/interactive-example/index.js
@@ -2,8 +2,7 @@ import _ from 'lodash';
 import testFragment from 'nebula-test-fragment';
 
 const {
-  Fragment,
-  Options
+  Fragment1
 } = testFragment;
 
 /**
@@ -16,30 +15,7 @@ const fragmentDisplayName = 'nebula-widgets.kitchen-sink.widgets.man-page.intera
 /**
  * Fragment that represents man page's interactive example.
  */
-class InteractiveExample extends Fragment {
-
-  /**
-   * Creates fragment.
-   *
-   * @param {InteractiveExample|Object} [spec] When it's already instance of `InteractiveExample` it would be returned as-is, otherwise it's same as extended fragment's constructor `spec` parameter
-   * @param {Options|Object} [opts] Options, same as extended fragment's constructor `opts` parameter
-   */
-  constructor(spec, opts) {
-    const {
-      initializedOpts,
-      initializedSpec,
-      isInstance
-    } = Fragment.initializeFragmentSpecAndOpts(spec, opts);
-
-    if (isInstance === true) {
-      return spec;
-    }
-
-    super(initializedSpec, initializedOpts);
-
-    return this;
-  }
-}
+class InteractiveExample extends Fragment1 {}
 
 Object.defineProperties(InteractiveExample, {
   bemBase: {
