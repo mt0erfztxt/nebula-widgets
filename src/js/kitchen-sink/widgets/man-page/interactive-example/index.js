ef08c92950bc9aa5c978d79ca7310dc62fae13a1
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-12-02 23:04:21 +0500

    added selector for 'view' element of man page's interactive example

--- src/js/kitchen-sink/widgets/man-page/interactive-example/index.js
+++ src/js/kitchen-sink/widgets/man-page/interactive-example/index.js
@@ -15,7 +15,38 @@ const fragmentDisplayName = 'nebula-widgets.kitchen-sink.widgets.man-page.intera
 /**
  * Fragment that represents man page's interactive example.
  */
-class InteractiveExample extends Fragment1 {}
+class InteractiveExample extends Fragment1 {
+
+  /**
+   * BEM base for fragment's 'view' element.
+   *
+   * @returns {BemBase}
+   */
+  get viewElementBemBase() {
+    if (!this._viewElementBemBase) {
+      this._viewElementBemBase = this
+        .cloneBemBase()
+        .setElt('view');
+    }
+
+    return this._viewElementBemBase;
+  }
+
+  /**
+   * TestCafe selector for fragment's 'view' element.
+   *
+   * @returns {Selector}
+   */
+  get viewElementSelector() {
+    if (!this._viewElementSelector) {
+      this._viewElementSelector = this
+        .selector
+        .find(`.${this.viewElementBemBase}`);
+    }
+
+    return this._viewElementSelector;
+  }
+}
 
 Object.defineProperties(InteractiveExample, {
   bemBase: {
