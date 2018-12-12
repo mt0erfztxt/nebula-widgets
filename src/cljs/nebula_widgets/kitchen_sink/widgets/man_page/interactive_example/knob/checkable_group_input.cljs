d02a1147f58d7824819e0765e4c34a756f0b5a26
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-12-13 02:18:20 +0500

    changed default for 'columns' prop of checkable group input knob of interactive example widget

--- src/cljs/nebula_widgets/kitchen_sink/widgets/man_page/interactive_example/knob/checkable_group_input.cljs
+++ src/cljs/nebula_widgets/kitchen_sink/widgets/man_page/interactive_example/knob/checkable_group_input.cljs
@@ -4,7 +4,7 @@
     [nebula-widgets.widgets.checkable-group-input.core :as checkable-group-input]))
 
 (def ^:private default-cgi-props
-  {:columns 8
+  {:columns 5
    :inline true
    :items (for [v [false true]] {:label (str v), :value v})
    :no-row-gap true
@@ -20,10 +20,14 @@
   Arguments:
   * `knobs-props` - required, map. See knob/widget for details.
   * `input-props` - optional, map. Props for checkable-group-input/widget, merged with defaults:
-    - `:columns` - 8
+    - `:columns` - 5
     - `:inline` - true
     - `:items` - two items to create boolean switch (false and true)
     - `:no-row-gap` - true
     - `:selection-mode` - single"
   [knob-props input-props]
   [knob/widget knob-props checkable-group-input/widget (merge default-cgi-props input-props)])
+
+(defn- gen-items [& labels]
+  (for [s labels]
+    {:label s, :value s}))
