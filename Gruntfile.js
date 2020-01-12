dc85d9df2f3530e0719a6ce63ef1db8ace419ba0
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2020-01-12 17:22:32 +0500

    Add SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const md5 = require("md5");
+const sass = require("dart-sass");
 
 module.exports = function(grunt) {
   function getCollectiveMd5(files) {
@@ -20,17 +21,34 @@ module.exports = function(grunt) {
       }
     },
     copy: {
-      fonts: {
+      mainFonts: {
         expand: true,
         cwd: "node_modules/font-awesome/fonts",
         src: "**/*",
         dest: "public/assets/fonts"
       },
-      images: {
+      mainImages: {
         expand: true,
-        cwd: "src/images",
+        cwd: "src/main/images",
         src: "**",
         dest: "public/assets/images"
+      }
+    },
+    sass: {
+      kitchenSink: {
+        files: {
+          "public/assets/css/kitchen-sink.css":
+            "src/kitchen-sink/sass/index.sass"
+        }
+      },
+      main: {
+        files: {
+          "public/assets/css/main.css": "src/main/sass/index.sass"
+        }
+      },
+      options: {
+        implementation: sass,
+        sourceMap: true
       }
     },
     sh: {
@@ -44,35 +62,47 @@ module.exports = function(grunt) {
       },
       kitchenSinkProdCssBuild: {
         cmd:
-          "mkdir -p resources/public/assets/css && ./node_modules/.bin/stylus --hoist-atrules -u autoprefixer-stylus -u axis -u jeet -u rupture --include-css -o public/assets/css/kitchen-sink.css ./src/stylus/kitchen-sink/index.styl && ./node_modules/.bin/cleancss -o public/assets/css/kitchen-sink.css public/assets/css/kitchen-sink.css"
+          "mkdir -p public/assets/css && ./node_modules/.bin/stylus --hoist-atrules -u autoprefixer-stylus -u axis -u jeet -u rupture --include-css -o public/assets/css/kitchen-sink.css ./src/stylus/kitchen-sink/index.styl && ./node_modules/.bin/cleancss -o public/assets/css/kitchen-sink.css public/assets/css/kitchen-sink.css"
       }
     },
     sprite: {
       icons: {
-        src: "src/icons/*.png",
+        src: "src/main/icons/*.png",
         dest:
-          "public/assets/images/icons-" +
+          "public/assets/images/sprites-" +
           getCollectiveMd5("assets/images/icons/*.png") +
           ".png",
-        destCss: "src/stylus/core/icons.styl",
+        destCss: "src/main/sass/_sprites.sass",
         imgPath:
           "../images/icons-" +
           getCollectiveMd5("assets/images/icons/*.png") +
           ".png"
       }
+    },
+    watch: {
+      files: ["src/kitchen-sink/sass/**/*.sass", "src/main/sass/**/*.sass"],
+      tasks: ["sass:kitchenSink"]
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-sass");
   grunt.loadNpmTasks("grunt-sh");
   grunt.loadNpmTasks("grunt-spritesmith");
 
   // -- Kitchen Sink
 
+  grunt.registerTask("kitchenSink:dev:watch", [
+    "kitchenSink:dev:assets:build",
+    "sass:kitchenSink",
+    "watch"
+  ]);
+
   grunt.registerTask("kitchenSink:dev:assets:build", [
-    "copy:fonts",
-    "copy:images"
+    "copy:mainFonts",
+    "copy:mainImages"
   ]);
 
   grunt.registerTask("kitchenSink:dev:css:build", [
